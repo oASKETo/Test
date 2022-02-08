aca82b73075b41f5ee95bc5a5762a8bebbb1cff7
oASKETo <kolua.ua99@mail.ru>
2022-02-08 23:27:42 +0300

    api

---
+++ todo/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Todo
+
+
+# Todo serializer
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+
+# Category serializer
+# class CategorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = '__all__'
