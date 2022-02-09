489f6508f041c4c73a4c64eb2653cbdae41a31ff
oASKETo <kolua.ua99@mail.ru>
2022-02-09 14:32:04 +0300

    final

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,16 +4,18 @@ import ReactDOM from 'react-dom';
 import Header from './layout/Header';
 import DashBoard from './todo/DashBoard';
 
-// import { Provider } from 'react-redux';
-// import store from '../store';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 class App extends Component {
     render() {
         return (
-            <Fragment>
-                <Header/>
-                <DashBoard/>
-            </Fragment>
+            <Provider store={store}>
+                <div className='container'>
+                    <Header />
+                    <DashBoard />
+                </div>
+            </Provider>
         )
     }
 }
